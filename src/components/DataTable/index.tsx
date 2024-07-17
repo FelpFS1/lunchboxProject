e05159281ce38b95f6e92bbd555273dbfebabce2
unknown <phff800@gmail.com>
2024-07-17 18:12:36 -0300

    add: Create Table component

---
+++ src/components/DataTable/index.tsx
@@ -0,0 +1,22 @@
+import { TableContainer } from "./styles";
+
+export default function DataTable() {
+  return (
+    <TableContainer>
+      <tbody>
+        <tr>
+          <td>aaaa</td>
+          <td>nnnn</td>
+          <td>cccc</td>
+          <td>oooo</td>
+        </tr>
+        <tr>
+          <td>aaaa</td>
+          <td>nnnn</td>
+          <td>cccc</td>
+          <td>oooo</td>
+        </tr>
+      </tbody>
+    </TableContainer>
+  );
+}
---
+++ src/components/DataTable/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const TableContainer = styled.table`
+  width: 85vw;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  margin-top: 1.5rem;
+  justify-content: center;
+  padding: 0 3.333vw;
+  overflow-x: auto;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${(props) => props.theme["gray-400"]};
+    border-right: 2px solid ${(props) => props.theme["gray-400"]};
+    border-left: 2px solid ${(props) => props.theme["gray-400"]};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    &:last-child {
+      text-align: right;
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
