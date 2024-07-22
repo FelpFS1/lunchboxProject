3163578ec99a491ec12c7f821cc2d3aa20c4b809
unknown <phff800@gmail.com>
2024-07-22 20:02:29 -0300

    Correction: DataTable became a reusable component

--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,22 +1,8 @@
+import { ReactNode } from "react";
 import { TableContainer } from "./styles";
-
-export default function DataTable() {
-  return (
-    <TableContainer>
-      <tbody>
-        <tr>
-          <td>OSMAR</td>
-          <td>VALE DAS PALMEIRAS</td>
-          <td>19 MARMITAS</td>
-          <td>08/07/2024</td>
-        </tr>
-        <tr>
-          <td>aaaa</td>
-          <td>nnnn</td>
-          <td>cccc</td>
-          <td>oooo</td>
-        </tr>
-      </tbody>
-    </TableContainer>
-  );
+interface DataTableProps {
+  children: ReactNode;
+}
+export default function DataTable({ children }: DataTableProps) {
+  return <TableContainer>{children}</TableContainer>;
 }
