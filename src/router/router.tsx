a61f44b7a85167913f59fbb27d2302349ecde8e3
unknown <phff800@gmail.com>
2024-07-23 18:05:37 -0300

    fix: router pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import RootLayout from "../layouts/RootLayout";
 import Clients from "../pages/Clients";
-import WeeklyReport from "../pages/WeeklyReport";
+import WeeklyReport from "../pages/Report";
 
 export const router = createBrowserRouter([
   {
